cdfb77ab3f7ed8805f1d7316a3548bc244ed48dd
devnagoyal-ux <58598904+devnagoyal-ux@users.noreply.github.com>
2020-12-12 20:51:56 +0530

    imported notification screen

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import { AppTabNavigator } from './AppTabNavigator'
 import CustomSideBarMenu  from './CustomSideBarMenu';
+import NotificationScreen from '../screens/NotificationScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 export const AppDrawerNavigator = createDrawerNavigator({
