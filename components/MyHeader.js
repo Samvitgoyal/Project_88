cd09eee625f802dba0378a4baef797b43731ffc2
devnagoyal-ux <58598904+devnagoyal-ux@users.noreply.github.com>
2020-12-12 20:46:07 +0530

    added icon badge and hamburger menu

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -1,14 +1,49 @@
 import React, { Component} from 'react';
-import { Header } from 'react-native-elements';
+import { Header,Icon,Badge } from 'react-native-elements';
 import { View, Text, StyeSheet ,Alert} from 'react-native';
+import db from '../config';
 
-const MyHeader = props => {
+export default class MyHeader extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      value : "",
+    }
+  }
+
+  getNumberOfUnreadNotifications(){
+    db.collection('all_notifications').where('notification_status','==',"unread")
+    .onSnapshot((snapshot)=>{
+      var unreadNotifications = snapshot.docs.map((doc) => doc.data())
+      this.setState({
+        value: unreadNotifications.length
+      })
+    })
+  }
+  
+  componentDidMount(){
+    this.getNumberOfUnreadNotifications()
+  }
+
+  BellIconWithBadge=()=>{
+    return(
+      <View>
+        <Icon name='bell' type='font-awesome' color='#696969' size={25}
+          onPress={() =>this.props.navigation.navigate('Notification')}/>
+         <Badge
+          value={this.state.value}
+         containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
+      </View>
+    )
+  }
+ render(){
   return (
     <Header
-      centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+      leftComponent = {<Icon name='bars' type='font-awesome' color='#696969' onPress={()=>props.navigation.CustomSideBarMenu()}/>}
+      centerComponent={{ text: this.props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+      rightComponent={<this.BellIconWithBadge {...this.props}/>}
       backgroundColor = "#eaf8fe"
     />
   );
-};
-
-export default MyHeader;
+}
+}
