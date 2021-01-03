a6504007c37d72181720f15e97e68de880ef259f
Samvitgoyal <76777983+Samvitgoyal@users.noreply.github.com>
2021-01-03 16:00:56 +0530

    Update ExchangeScreen.js

--- screens/ExchangeScreen.js
+++ screens/ExchangeScreen.js
@@ -1,289 +1,195 @@
-import React,{Component} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  KeyboardAvoidingView,
-  StyleSheet,
-  ScrollView,
-  TouchableOpacity,
-  Alert} from 'react-native';
-import db from '../config';
+import React ,{Component} from 'react';
+import {View,Text,StyleSheet,TouchableOpacity} from 'react-native';
+import{Card,Header,Icon} from 'react-native-elements';
 import firebase from 'firebase';
-import MyHeader from '../components/MyHeader'
 
-export default class ExchangeScreen extends Component{
-  constructor(){
-    super();
-    this.state ={
-      userId : firebase.auth().currentUser.email,
-      itemName:"",
-      itemDescription:"",
-      IsExchangedRequestActive : "",
-      requestedItemName: "",
-      itemStatus:"",
-      requestId:"",
-      userDocId: '',
-      docId :''
+import db from '../config.js';
+
+export default class RecieverDetailsScreen extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      userId          : firebase.auth().currentUser.email,
+      userName        : "",
+      recieverId      : this.props.navigation.getParam('details')["user_id"],
+      requestId       : this.props.navigation.getParam('details')["request_id"],
+      itemName        : this.props.navigation.getParam('details')["book_name"],
+      item_description     : this.props.navigation.getParam('details')["reason_to_request"],
+      recieverName    : '',
+      recieverContact : '',
+      recieverAddress : '',
+      recieverRequestDocId : '',
+      currencyCode : ''
     }
   }
 
-  createUniqueId(){
-    return Math.random().toString(36).substring(7);
-  }
 
 
-
-  addRequest = async (itemName,itemDescription)=>{
-    var userId = this.state.userId
-    var randomRequestId = this.createUniqueId()
-    db.collection('requested_items').add({
-        "user_id": userId,
-        "item_name":itemName,
-        "item_description":itemDescription,
-        "request_id"  : randomRequestId,
-        "item_status" : "received",
-         "date"       : firebase.firestore.FieldValue.serverTimestamp()
-
-    })
-
-    await  this.getItemRequest()
-    db.collection('users').where("email_id","==",userId).get()
-    .then()
-    .then((snapshot)=>{
-      snapshot.forEach((doc)=>{
-        db.collection('users').doc(doc.id).update({
-      IsItemRequestActive: true
-      })
-    })
-  })
-
-   this.setState({
-        itemName :'',
-        itemDescription : ''
-    })
-
-    return Alert.alert( 
-      'Item ready to exchange ',
-    '',
-    [
-      {text : 'Ok',onPress:()=>{
-      this.props.navigation.navigate('HomeScreen')
-      }}
-      ]
-      );
-  }
-
-  receivedItems=(itemName)=>{
-    var userId = this.state.userId
-    var requestId = this.state.requestId
-    db.collection('received_items').add({
-        "user_id": userId,
-        "item_name":itemName,
-        "request_id"  : requestId,
-        "itemStatus"  : "received",
-  
-    })
-  }
-  
-   getIsExchangedRequestActive(){
-    db.collection('users')
-    .where('email_id','==',this.state.userId)
-    .onSnapshot(querySnapshot => {
-      querySnapshot.forEach(doc => {
+  getRecieverDetails(){
+    db.collection('users').where('email_id','==',this.state.recieverId).get()
+    .then(snapshot=>{
+      snapshot.forEach(doc=>{
         this.setState({
-          IsExchangedRequestActive:doc.data().IsExchangedRequestActive,
-          userDocId : doc.id
+          recieverName    : doc.data().first_name,
+          recieverContact : doc.data().contact,
+          recieverAddress : doc.data().address,
         })
       })
-    })
-  }
-  
-  
-  getItemRequest =()=>{
-    // getting the requested book
-  var itemRequest=  db.collection('requested_items')
-    .where('user_id','==',this.state.userId)
-    .get()
-    .then((snapshot)=>{
-      snapshot.forEach((doc)=>{
-        if(doc.data().item_status !== "received"){
-          this.setState({
-            requestId : doc.data().request_id,
-            requestedItemName: doc.data().item_name,
-            itemStatus:doc.data().item_status,
-            docId     : doc.id
-          })
-        }
-      })
+    });
+
+    db.collection('requested_books').where('request_id','==',this.state.requestId).get()
+    .then(snapshot=>{
+      snapshot.forEach(doc => {
+        this.setState({recieverRequestDocId:doc.id})
+     })
   })}
-  
-  
-  
-  sendNotification=()=>{
-    //to get the first name and last name
-    db.collection('users').where('email_id','==',this.state.userId).get()
-    .then((snapshot)=>{
-      snapshot.forEach((doc)=>{
-        var name = doc.data().first_name
-        var lastName = doc.data().last_name
-  
-        // to get the donor id and book nam
-        db.collection('all_notifications').where('request_id','==',this.state.requestId).get()
-        .then((snapshot)=>{
-          snapshot.forEach((doc) => {
-            var donorId  = doc.data().donor_id
-            var itemName =  doc.data().item_name
-  
-            //targert user id is the donor id to send notification to the user
-            db.collection('all_notifications').add({
-              "targeted_user_id" : donorId,
-              "message" : name +" " + lastName + " received the item " + itemName ,
-              "notification_status" : "unread",
-              "item_name" : itemName
-            })
-          })
-        })
-      })
-    })
-  }
-  
-  componentDidMount(){
-    this.getItemRequest()
-    this.getIsExchangedRequestActive()
-  
-  }
-  
-  updateItemRequestStatus=()=>{
-    //updating the book status after receiving the book
-    db.collection('requested_items').doc(this.state.docId)
-    .update({
-      item_status : 'recieved'
-    })
-  
-    //getting the  doc id to update the users doc
-    db.collection('users').where('email_id','==',this.state.userId).get()
+
+
+  getUserDetails=(userId)=>{
+    db.collection("users").where('email_id','==', userId).get()
     .then((snapshot)=>{
       snapshot.forEach((doc) => {
-        //updating the doc
-        db.collection('users').doc(doc.id).update({
-          IsExchangedRequestActive: false
+        this.setState({
+          userName  :doc.data().first_name + " " + doc.data().last_name
         })
       })
     })
-  
-  
-  }  
+  }
+
+  getData(){
+    fetch("http://data.fixer.io/api/latest?access_key=1f7dd48123a05ae588283b5e13fae944&format=1")
+    .then(response=>{
+      return response.json();
+    }).then(responseData =>{
+      var currencyCode = this.state.currencyCode
+      var currency = responseData.rates.INR
+      var value = 69/currency
+      console.log(value);
+    })
+}
+
+  updateItemStatus=()=>{
+    db.collection('all_donations').add({
+      "item_name"           : this.state.itemName,
+      "request_id"          : this.state.requestId,
+      "requested_by"        : this.state.recieverName,
+      "donor_id"            : this.state.userId,
+      "request_status"      :  "Donor Interested"
+    })
+  }
 
 
-  render(){
-    
-      if(this.state.IsExchangedRequestActive === true){
-        return(
-  
-          // Status screen
-  
-          <View style = {{flex:1,justifyContent:'center'}}>
-            <View style={{borderColor:"orange",borderWidth:2,justifyContent:'center',alignItems:'center',padding:10,margin:10}}>
-            <Text>Item Name</Text>
-            <Text>{this.state.requestedItemName}</Text>
-            </View>
-            <View style={{borderColor:"orange",borderWidth:2,justifyContent:'center',alignItems:'center',padding:10,margin:10}}>
-            <Text> Item Status </Text>
-  
-            <Text>{this.state.itemStatus}</Text>
-            </View>
-  
-            <TouchableOpacity style={{borderWidth:1,borderColor:'orange',backgroundColor:"orange",width:300,alignSelf:'center',alignItems:'center',height:30,marginTop:30}}
-            onPress={()=>{
-              this.sendNotification()
-              this.updateItemRequestStatus();
-              this.receivedItems(this.state.requestedItemName)
-            }}>
-            <Text>I recieved the item</Text>
-            </TouchableOpacity>
-          </View>
-        )
-      }
-      else
-      {
+  addNotification=()=>{
+    var message = this.state.userName + " has shown interest in exchanging the item"
+    db.collection('all_notifications').add({
+      "targeted_user_id"    : this.state.recieverId,
+      "donor_id"            : this.state.userId,
+      "request_id"          : this.state.requestId,
+      "item_name"           : this.state.itemName,
+      "date"                : firebase.firestore.FieldValue.serverTimestamp(),
+      "notification_status" : "unread",
+      "message"             : message
+    })
+  }
+
+
+
+  componentDidMount(){
+    this.getRecieverDetails()
+    this.getUserDetails(this.state.userId)
+  }
+
+
+    render(){
       return(
-        // Form screen
-          <View style={{flex:1}}>
-            <MyHeader title="Exchange Items" navigation ={this.props.navigation}/>
-  
-            <ScrollView>
-              <KeyboardAvoidingView style={styles.keyBoardStyle}>
-                <TextInput
-                  style ={styles.formTextInput}
-                  placeholder={"enter item name"}
-                  onChangeText={(text)=>{
-                      this.setState({
-                          itemName:text
-                      })
-                  }}
-                  value={this.state.itemName}
-                />
-                <TextInput
-                  style ={[styles.formTextInput,{height:300}]}
-                  multiline
-                  numberOfLines ={8}
-                  placeholder={"Description of an item"}
-                  onChangeText ={(text)=>{
-                      this.setState({
-                          item_description:text
-                      })
-                  }}
-                  value ={this.state.item_description}
-                />
-                <TouchableOpacity
-                  style={styles.button}
-                  onPress={()=>{ this.addRequest(this.state.itemName,this.state.item_description);
-                  }}
-                  >
-                  <Text>Request</Text>
-                </TouchableOpacity>
-  
-              </KeyboardAvoidingView>
-              </ScrollView>
+        <View style={styles.container}>
+          <View style={{flex:0.1}}>
+            <Header
+              leftComponent ={<Icon name='arrow-left' type='feather' color='#696969'  onPress={() => this.props.navigation.goBack()}/>}
+              centerComponent={{ text:"Exchange Items", style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+              backgroundColor = "#eaf8fe"
+            />
           </View>
+          <View style={{flex:0.3}}>
+            <Card
+                title={"Item Information"}
+                titleStyle= {{fontSize : 20}}
+              >
+              <Card >
+                <Text style={{fontWeight:'bold'}}>Name : {this.state.itemName}</Text>
+              </Card>
+              <Card>
+                <Text style={{fontWeight:'bold'}}>Value:{this.state.currencyCode}</Text>
+              </Card>
+              <Card>
+                <Text style={{fontWeight:'bold'}}>Description : {this.state.item_description}</Text>
+              </Card>
+            </Card>
+          </View>
+          <View style={{flex:0.3}}>
+            <Card
+              title={"Reciever Information"}
+              titleStyle= {{fontSize : 20}}
+              >
+              <Card>
+                <Text style={{fontWeight:'bold'}}>Name: {this.state.recieverName}</Text>
+              </Card>
+              <Card>
+                <Text style={{fontWeight:'bold'}}>CurrencyCode:{this.state.currencyCode}</Text>
+              </Card>
+              <Card>
+                <Text style={{fontWeight:'bold'}}>Contact: {this.state.recieverContact}</Text>
+              </Card>
+              <Card>
+                <Text style={{fontWeight:'bold'}}>Address: {this.state.recieverAddress}</Text>
+              </Card>
+            </Card>
+          </View>
+          <View style={styles.buttonContainer}>
+            {
+              this.state.recieverId !== this.state.userId
+              ?(
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={()=>{
+                      this.updateItemStatus()
+                      this.addNotification()
+                      this.props.navigation.navigate('MyBarters')
+                    }}>
+                  <Text>Exchange</Text>
+                </TouchableOpacity>
+              )
+              : null
+            }
+          </View>
+        </View>
       )
     }
-  }
-  }
+
+}
+
 
 const styles = StyleSheet.create({
-  keyBoardStyle : {
+  container: {
     flex:1,
-    alignItems:'center',
-    justifyContent:'center'
   },
-  formTextInput:{
-    width:"75%",
-    height:35,
-    alignSelf:'center',
-    borderColor:'#ffab91',
-    borderRadius:10,
-    borderWidth:1,
-    marginTop:20,
-    padding:10,
+  buttonContainer : {
+    flex:0.3,
+    justifyContent:'center',
+    alignItems:'center'
   },
   button:{
-    width:"75%",
+    width:200,
     height:50,
     justifyContent:'center',
-    alignItems:'center',
-    borderRadius:10,
-    backgroundColor:"#ff5722",
+    alignItems : 'center',
+    borderRadius: 10,
+    backgroundColor: 'orange',
     shadowColor: "#000",
     shadowOffset: {
        width: 0,
-       height: 8,
-    },
-    shadowOpacity: 0.44,
-    shadowRadius: 10.32,
-    elevation: 16,
-    marginTop:20
-    },
+       height: 8
+     },
+    elevation : 16
   }
-)
+})
